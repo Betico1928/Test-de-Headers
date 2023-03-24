651861bc019a482e7721e4af98e519f67600d95e
Betico1928 <betoveluis@gmail.com>
2023-03-23 22:58:15 -0500

    Creados los prototipos de las funciones.

--- Operaciones.h
+++ Operaciones.h
@@ -5,4 +5,10 @@
 #ifndef CREACION_DE_HEADERS_OPERACIONES_H
 #define CREACION_DE_HEADERS_OPERACIONES_H
 
+// Prototipo de funciones
+int funcionSuma (int datoA, int datoB);
+int funcionResta (int datoA, int datoB);
+int funcionMultiplicacion (int datoA, int datoB);
+int funcionDivision (int datoA, int datoB);
+
 #endif //CREACION_DE_HEADERS_OPERACIONES_H
